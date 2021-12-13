802deb2e300ed801ad9613de53ae23bd610d8b51
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2021-12-13 17:15:50 +0530

    Time: 96 ms (90.47%), Space: 67.7 MB (91.19%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        // if(nums.size()==1) return nums[0];
+        
+        int max_so_far = a[0];
+        int curr_max = a[0];
+ 
+        for (int i = 1; i < a.size(); i++)
+        {
+            curr_max = max(a[i], curr_max+a[i]);
+            max_so_far = max(max_so_far, curr_max);
+        }
+        return max_so_far;
+        
+        
+    }
+};
