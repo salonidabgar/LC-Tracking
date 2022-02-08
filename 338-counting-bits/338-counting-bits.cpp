0adc8916a700d8c899da919d919cffdb416de2a4
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-02-09 04:31:17 +0530

    Time: 4 ms (89.23%), Space: 7.9 MB (48.87%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     
-    vector<int> countBits(int num) {
-      int count=0;
+    vector<int> countBits(int n) {
+//       int count=0;
 
-    vector<int>ans;
-    for(int i=0;i<=num;i++)
-    {
-        count=0;
-        for(int j=i;j>0;)
+//     vector<int>ans;
+//     for(int i=0;i<=num;i++)
+//     {
+//         count=0;
+//         for(int j=i;j>0;)
+//         {
+//             j=j&(j-1);
+//             count++;
+//         }
+//          ans.push_back(count);
+//     }
+//     return ans;
+        
+        
+        vector<int> res(n+1,0);
+        for(int i=1;i<=n;i++)
         {
-            j=j&(j-1);
-            count++;
+            res[i] = res[i &(i-1)] +1;
         }
-         ans.push_back(count);
-    }
-    return ans;
+        return res;
     }
 };
