42c0362aed0897dcb55709d51d2821f1d3301adb
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-02-09 23:03:54 +0530

    Time: 54 ms (17.15%), Space: 16.6 MB (24.58%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> sol;
+    void path(vector<vector<int>> & g, int idx,vector<int> currpath)
+    {
+        
+        // cout<<g[idx].size() <<endl;
+        int n = g.size();
+        currpath.push_back(idx);
+        
+        // if(idx >= g[idx].size()) return sol;
+        if(idx == n-1 ) 
+        {
+            sol.push_back(currpath);
+            return;
+        }
+        for(int j = 0; j<g[idx].size();j++)
+        {
+            
+            //currpath.push_back(g[idx][j]);
+            path(g,g[idx][j] , currpath);
+            
+            
+        }
+        currpath.pop_back();
+        
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> currpath;
+        path(graph, 0, currpath);
+        return sol;
+    }
+};
