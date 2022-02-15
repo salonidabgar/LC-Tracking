ea8e96126366e5d877ab6abf5aeb8e56e9edce5b
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-02-15 17:12:05 +0530

    Time: 12 ms (98.32%), Space: 16.9 MB (85.89%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       unordered_map<int, int> mp;
-        for(auto i: nums)
-        {
-            mp[i]++;
-        }
+//        unordered_map<int, int> mp;
+//         for(auto i: nums)
+//         {
+//             mp[i]++;
+//         }
         
-        for(auto x: mp)
+//         for(auto x: mp)
+//         {
+//             if(x.second == 1) return x.first;
+//         }
+//         return -1;
+        int res = 0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(x.second == 1) return x.first;
+            res = res xor nums[i];
         }
-        return -1;
+        return res;
     }
 };
