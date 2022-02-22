16dd87857f2cc94665595f8dc7f15173a786e8ef
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-02-22 12:44:26 +0530

    Time: 4 ms (61.50%), Space: 6.6 MB (63.87%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> cand ;
+        vector<bool> visited(256, false);
+        for (char c : s)
+            cand[c]++;
+        string result = "0";
+        for (char c : s) {
+            cand[c]--;
+            if (visited[c]) continue;
+            while (c < result.back() && cand[result.back()]) {
+                visited[result.back()] = false;
+                result.pop_back();
+            }
+            result += c;
+            visited[c] = true;
+        }
+        return result.substr(1);
+        
+    }
+};
