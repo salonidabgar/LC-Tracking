8326f1bf0081e85d415bae0d0a008d1487fc96ae
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-03-01 16:10:28 +0530

    Time: 7 ms (62.32%), Space: 8.6 MB (21.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     
     vector<int> countBits(int n) {
-//       int count=0;
-
-//     vector<int>ans;
-//     for(int i=0;i<=num;i++)
-//     {
-//         count=0;
-//         for(int j=i;j>0;)
-//         {
-//             j=j&(j-1);
-//             count++;
-//         }
-//          ans.push_back(count);
-//     }
-//     return ans;
-        
-        
-        vector<int> res(n+1,0);
-        for(int i=1;i<=n;i++)
+         vector<int> ans;
+		
+		// iterating fromt 0 to n
+        for(int i = 0; i<=n; i++)
         {
-            res[i] = res[i &(i-1)] +1;
+			// sum is initialised as 0
+            int sum = 0;
+            int num = i;
+			// while num not equals zero
+            while(num != 0)
+            {
+				// we have to count 1's in binary representation of i, therefore % 2
+                sum += num%2;
+                num = num/2;
+            }
+			// add sum to ans vector
+            ans.push_back(sum);
         }
-        return res;
+		// return 
+        return ans;
     }
 };
