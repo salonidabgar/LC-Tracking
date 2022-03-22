5ea22ada2ec0933d3313c4283c8e0834f5097a11
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-03-22 18:19:35 +0530

    Time: 48 ms (61.48%), Space: 19.2 MB (44.47%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> sol;
+        sort(intervals.begin(), intervals.end());
+        vector<int> ans ={intervals[0][0], intervals[0][1]};
+        int n = intervals.size(), m = intervals[0].size();
+        if(n==1) 
+        {
+            sol.push_back(ans);
+            return sol;
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(intervals[i][0]<=ans[1])
+            {
+                if(intervals[i][0]<ans[0])
+                {
+                    ans[0] = intervals[i][0];
+                }
+                if(intervals[i][1] >ans[1])
+                {
+                    ans[1] = intervals[i][1];
+                }
+            }
+            else
+            {
+                sol.push_back(ans);
+                ans = {intervals[i][0], intervals[i][1]};
+                // continue;
+            }
+            
+        }
+        sol.push_back(ans);
+        return sol;
+    }
+};
