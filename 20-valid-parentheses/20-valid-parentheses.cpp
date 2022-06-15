7f01accf15517560d2c3f8f9f2d55ad42a7a2bc3
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-06-15 17:16:38 +0530

    Time: 4 ms (30.89%), Space: 6.4 MB (51.00%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.length()%2!=0 ) return false;
         stack<char> st;
-        int i;
-        for(i=0;i<s.length();i++)
+        for(char c: s)
         {
-            // cout<<s[i]<<" "<<st.top()<<endl;
-            if(s[i] == '(' or s[i] == '{' or s[i] == '[')
-            {
-                st.push(s[i]);
-            }
-            else if(s[i] == ')' and  !st.empty() and st.top() == '(')
-            {
-                st.pop();
-                continue;
-            }
-            else if(s[i] == '}' and !st.empty() and st.top() == '{')
-            {
-                st.pop();
-                continue;
-            }
-            else if(s[i] == ']' and !st.empty() and st.top() == '[')
-            {
-                st.pop();
-                continue;
-            }
-            else return false;
-            }
-        if(st.empty()) return true;
-        else return false;
-
+            if(c == '(') st.push(')');
+            else if(c == '{') st.push('}');
+            else if(c == '[') st.push(']');
+            else if(st.empty()  or st.top()!=c) return false;
+            else st.pop();
+            
+        }
+        return st.empty();
     }
+        
+    
 };
