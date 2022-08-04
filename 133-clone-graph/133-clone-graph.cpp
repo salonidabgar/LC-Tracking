b27d1a022f671d977b6a6a46e40a6e5ae9dfc15a
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-08-04 20:47:55 +0530

    Time: 15 ms (19.03%), Space: 9 MB (34.55%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -1,109 +1,70 @@
-// /*
-// // Definition for a Node.
-// class Node {
-// public:
-//     int val;
-//     vector<Node*> neighbors;
-//     Node() {
-//         val = 0;
-//         neighbors = vector<Node*>();
-//     }
-//     Node(int _val) {
-//         val = _val;
-//         neighbors = vector<Node*>();
-//     }
-//     Node(int _val, vector<Node*> _neighbors) {
-//         val = _val;
-//         neighbors = _neighbors;
-//     }
-// };
-// */
-
-
-// class Solution {
-//     void dfs(Node* curr, Node* node, vector<Node*>& visited)
-//     {
-//         visited[node->val] = node;
-//         for(auto j: curr->neighbors)
-//         {
-//             if(visited[j->val] == NULL)
-//             {
-//                 Node* newnode = new Node(j->val);
-//                 (node->neighbors).push_back(visited[j->val]);
-//                 dfs(j, newnode, visited);
-//             }
-//             else
-//             {
-//                 (node->neighbors).push_back(visited[j->val]);
-//             }
-//         }
-//     }
-// public:
-    
-//     Node* cloneGraph(Node* node) {
-//         if(node == NULL) return NULL;
-        
-//         vector<Node*> visited(1000, NULL);
-//         Node* copy = new Node(node->val);
-//         visited[node->val] = copy;
-        
-//         for(auto i: node->neighbors)
-//         {
-//             if(visited[i->val] == NULL)
-//             {
-//                 Node* newnode = new Node(i->val);
-//                 (copy->neighbors).push_back(newnode);
-//                 dfs(i, newnode, visited);
-//             }
-//             else
-//             {
-//                 (copy->neighbors).push_back(visited[i->val]);
-//             }
-//         }
-//         return copy;
-//     }
-// };
-
-class Solution {
-    void dfs(Node* curr,Node* node,vector<Node *>& visited)
-    {
-        //Node* copy = new Node(node->val);
-        visited[node->val] = node;
-        for(auto ele: curr->neighbors)
-        {
-            if(visited[ele->val] == NULL)
-            {
-                Node *newnode = new Node(ele->val);
-                (node->neighbors).push_back(newnode);
-                dfs(ele,newnode,visited);
-            }
-            else
-                (node->neighbors).push_back(visited[ele->val]);
-        }
-    }
+/*
+// Definition for a Node.
+class Node {
 public:
-    Node* cloneGraph(Node* node) {
-        if(node==NULL)
-            return NULL;
-        
-        vector<Node *> visited(1000,NULL);
-        Node* copy = new Node(node->val);
-        visited[node->val] = copy;
-        //Iterate for all neighbors
-        for(auto curr: node->neighbors)
-        {
-            if(visited[curr->val] == NULL)
-            {
-                Node *newnode = new Node(curr->val);
-                (copy->neighbors).push_back(newnode);
-                dfs(curr,newnode,visited);
-            }
-            else
-                (copy->neighbors).push_back(visited[curr->val]);
-        }
-        return copy;
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
     }
 };
+*/
 
-
-
+class Solution {
+    void dfs(Node* curr, Node* node, vector<Node*> &vis)
+    {
+        vis[node->val] = node;
+        for(auto nei: curr->neighbors )
+        {
+            if(vis[nei->val] == NULL)
+            {
+                Node* newnode = new Node(nei->val);
+                (node->neighbors).push_back(newnode);
+                dfs(nei, newnode, vis);
+            }
+            else
+            {
+                (node->neighbors).push_back(vis[nei->val]);
+            }
+        }
+        
+        
+    }
+    
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return NULL;
+        // to store wether or not that particular node is created
+        vector<Node*> vis(1000, NULL);
+        
+        Node *copy = new Node(node->val);
+        vis[node->val] = copy;
+        
+        for(auto nei: node->neighbors)
+        {
+            if(vis[nei->val] == NULL)
+            {
+                Node* newnode = new Node(nei->val);
+                // vis[nei->val] = nei;
+                (copy->neighbors).push_back(newnode);
+                dfs(nei, newnode, vis);
+            }
+            else
+            {
+                (copy->neighbors).push_back(vis[nei->val]);
+            }
+        }
+        
+        return copy;
+        
+    }
+};
