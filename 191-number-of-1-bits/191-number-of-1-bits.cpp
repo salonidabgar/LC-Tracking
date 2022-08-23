2627f330900bd1f4c02895f2aac61fd85b4de9ed
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-08-23 16:49:51 +0530

    Time: 8 ms (8.55%), Space: 5.9 MB (47.96%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count =0;
-        while(n)
-        {
-            n = n&(n-1);
-            count++;
-        }
-        return count;
+        
+        return __builtin_popcount(n);
     }
 };
