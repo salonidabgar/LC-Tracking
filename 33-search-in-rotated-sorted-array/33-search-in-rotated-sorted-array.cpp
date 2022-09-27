f6255676b08ac1ddbe84be585d494ff9df571dea
Saloni Dabgar <58362719+salonidabgar@users.noreply.github.com>
2022-09-28 03:23:54 +0530

    Time: 5 ms (66.03%), Space: 11.1 MB (29.16%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,39 +1,54 @@
 class Solution {
 public:
-
-    int search(vector<int>& nums, int target) {
-        int lo=0,hi= nums.size()-1;   
+    int BinarySearch(vector<int> & nums, int target, int lo, int hi)
+    {
         while(lo<=hi)
         {
-            int mid = lo+(hi-lo)/2;
+            int mid = lo + (hi-lo)/2;
             if(nums[mid] == target)
             {
+                // cout<< mid<< endl;
                 return mid;
             }
-            else if(nums[lo]<=nums[mid])
+            else if(nums[mid] > target)
             {
-                if(target<=nums[mid] and target>=nums[lo])
-                {
-                    hi = mid-1;
-                }
-                else
-                {
-                    lo = mid+1;
-                }
+                hi = mid-1;
             }
             else
             {
-                if(target>=nums[mid] and target<=nums[hi])
-                {
-                    lo = mid+1;
-                }
-                else
-                {
-                    hi = mid-1;
-                }
+                lo = mid+1;
             }
-           
         }
-         return -1;
+        return -1;
     }
-    };
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l=0, r=n-1,pivot=0;
+        while(l<r)
+        {
+            int mid = l+(r-l)/2;
+            // if(nums[mid] == nums[r])
+            // {
+            //     pivot = mid;
+            //     break;
+            // }
+            if(nums[mid] < nums[r])
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+        pivot = l;
+        // cout << pivot<< endl;
+        if(target > nums[pivot] and target > nums[n-1])
+        {
+            return BinarySearch(nums, target, 0, pivot-1);
+        }
+        return BinarySearch(nums, target, pivot, n-1);
+        // return -1;
+            
+    }
+};
